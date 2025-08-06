406eecaff5040badf73d9fa9060f18bb9da1a6b9
Adam-Color <adamjoe02@gmail.com>
2025-08-06 15:19:10 -0400

    start of main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
+import pygame
+from pygame.locals import FULLSCREEN
+
+# constant vars
+SCREEN_X = 1920
+SCREEN_Y = 1080
+
+
 def main():
-    print("Hello World!")
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y), FULLSCREEN)
 
 
 if __name__ == "__main__":
